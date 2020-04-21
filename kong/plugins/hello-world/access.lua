39f48791054d3cb6d7e8b5ae7e4d83d153c06f82
virajrajput96 <64018156+virajrajput96@users.noreply.github.com>
2020-04-21 20:52:42 +0530

    Update access.lua

--- kong/plugins/hello-world/access.lua
+++ kong/plugins/hello-world/access.lua
@@ -1,1 +1,13 @@
+local _M = {}
 
+function _M.execute(conf)
+  if conf.sayto_hello then
+    ngx.log(ngx.ERR,"-------------HelloWorld------------")
+    ngx.header["Hello-World"] = "Hello World!!!!!"
+  else
+    ngx.log(ngx.ERR,"--------------ByeDuniya-----------")
+    ngx.header["Hello-World"]= "Bye World"
+  end
+  
+end
+return _M
